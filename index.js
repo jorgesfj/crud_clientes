847eca3fd4d3a4afa70f1290b1ab843e2d2b7d4d
Jorge Soares <junior-soares42@hotmail.com>
2021-09-03 13:55:28 -0300

    adicionando apis

--- index.js
+++ index.js
@@ -29,6 +29,24 @@ app.get("/", (req,res) => {
     });
 });
 
+
+//Buscar todos os clientes API
+app.get("/api/clientes", (req,res) => {
+    var sql = "SELECT * FROM clientes";
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+
+        res.json({
+            "message":"success",
+            "data":rows
+        });
+    });
+});
+
 //Buscar cliente por ID
 app.post("/clientes/id", (req,res) => {
     var sql = "SELECT * FROM clientes WHERE id = ?";
@@ -40,6 +58,22 @@ app.post("/clientes/id", (req,res) => {
         }
         console.log(row)
         res.render('encontrar_cliente', {cliente:row});
+    });
+});
+
+//Buscar cliente por Id API
+app.get("/api/cliente/:id", (req,res) => {
+    var sql = "SELECT * FROM clientes WHERE id = ?";
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
     });
 });
 
@@ -86,6 +120,52 @@ app.post("/clientes/create", (req,res) => {
     }); 
 });
 
+//Criar novo cliente API
+app.post("/api/cliente/", (req, res, next) => {
+    var errors=[]
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = new Date();
+    var month;
+    var day;
+    if(data.getMonth()+1 < 10){
+        month = "0"+(data.getMonth()+1);
+    }else{ month = data.getMonth()+1}
+    if(data.getDate() < 10){
+        day = "0"+data.getDate();
+    }else{ day = data.getDate()}
+
+    data_hoje = data.getFullYear()+"-"+month+"-"+day;
+  
+    if(errors.length){
+        res.status(400).json({"error": erros.json(",")});
+        return;
+    }
+    //codigo, nome, sobrenome, idade, data_criacao, data_atualizacao
+    var data  = {
+        nome:req.body.nome,
+        sobrenome: req.body.sobrenome,
+        idade: req.body.idade,
+        data_criacao: data_hoje,
+        data_atualizacao: data_hoje
+    }
+    var sql = 'INSERT INTO clientes ( nome, sobrenome, idade, data_criacao, data_atualizacao) VALUES (?,?,?,?,?)';
+    var params = [data.nome, data.sobrenome, data.idade, data.data_criacao, data.data_atualizacao];
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+
 app.post("/atualizar", (req,res) => {
     var cliente = {
         id: req.body.id,
@@ -97,7 +177,7 @@ app.post("/atualizar", (req,res) => {
     res.render('atualizar_cliente', {cliente: cliente});
 });
 
-//Atualizar Cliente - Nao utilizeo o app.put pois nem todos os navegadores tem suporte
+//Atualizar Cliente - Nao utilizei o app.put pois nem todos os navegadores tem suporte
 app.post("/atualizar/cliente", (req, res) => {
     var data = new Date();
     var month;
@@ -139,6 +219,50 @@ app.post("/atualizar/cliente", (req, res) => {
     );
 });
 
+//Atualizaar cliente API
+app.patch("/api/clientes/:id", (req, res, next) => {
+    var data = new Date();
+    var month;
+    var day;
+    if(data.getMonth()+1 < 10){
+        month = "0"+(data.getMonth()+1);
+    }else{ month = data.getMonth()+1}
+    if(data.getDate() < 10){
+        day = "0"+data.getDate();
+    }else{ day = data.getDate()}
+
+    data_hoje = data.getFullYear()+"-"+month+"-"+day;
+
+    var data = {
+        id: req.body.id,
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        idade: req.body.idade,
+        data_criacao: req.body.data_criacao,
+        data_atualizacao: data_hoje
+    }
+    db.run(
+        `UPDATE clientes SET
+        nome = COALESCE(?,nome),
+        sobrenome = COALESCE(?, sobrenome),
+        idade = COALESCE(?, idade),
+        data_criacao = COALESCE(?, data_criacao),
+        data_atualizacao = COALESCE(?, data_atualizacao)
+        WHERE id = ?`,
+        [data.nome, data.sobrenome, data.idade, data.data_criacao, data.data_atualizacao, data.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+});
+
 //Deletar Cliente - Nao utilzei o app.delete, pois nem todos os navegadores tem suporte.
 app.get('/clientes/delete/:id', (req,res) => {
     db.run(
@@ -153,6 +277,20 @@ app.get('/clientes/delete/:id', (req,res) => {
         }
     );
 });
+
+//Deletar Cliente API
+app.delete("/api/user/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM clientes WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+    });
+})
 
 app.get('/buscar/cliente', (req,res) => {
     res.render("encontrar_cliente", {cliente: "null"});
--- readme.md
+++ readme.md
@@ -16,6 +16,23 @@ O NodeJs pode ser instalado através deste link: https://nodejs.org/en/
 ## ⚙️ Executando
 Executar um projeto NodeJs normalmente 
 
+## Rotas da Api
+Buscar Todos os clientes:
+   GET http://localhost:8080/api/clientes
+
+Buscar cliente por id:
+    GET http://localhost:8080/api/cliente/ID
+
+Criar cliente:
+    POST http://localhost:8080/api/cliente/
+
+Atualizar Cliente:
+    PATCH http://localhost:8080/api/clientes/ID
+
+Deletar Cliente:
+    http://localhost:8080/api/user/13
+
+
 ## ✒️ Autor
 Jorge Soares – jorge.farias@arapiraca.ufal.br
 
